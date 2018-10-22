0868b6dfbdef65e8b1d7fe61098bfc22a434ba9f
xuexuguang <xuexuguang@renrenche.com>
2018-10-22 21:59:53 +0800

    项目基础环境部署

---
+++ app/Http/Controllers/ExampleController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class ExampleController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+
+    public function test()
+    {
+        return 'test';
+    }
+
+    //
+}
